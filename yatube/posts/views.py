d2a462ddd2c2f6dedb45cc297347339fbd14ae02
Аманов Молдаш <amanov1990@yandex.ru>
2023-11-19 21:48:32 +0500

    Добавление главное страницы

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Post
+
+from django.http import HttpResponse
+
+def index(request):
+    latest = Post.objects.order_by("-pub_date")[:10]
+    output = []
+    for i in latest:
+        output.append(i.text)
+    return HttpResponse("\n".join(output))
+
