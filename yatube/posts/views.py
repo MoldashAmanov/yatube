aae1306bcc672f9df09cc9867a2fb6e9545487aa
Аманов Молдаш <amanov1990@yandex.ru>
2023-12-09 02:57:58 +0500

    Добавил шаблон главной страницы

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -5,9 +5,6 @@ from .models import Post
 from django.http import HttpResponse
 
 def index(request):
-    latest = Post.objects.order_by("-pub_date")[:10]
-    output = []
-    for i in latest:
-        output.append(i.text)
-    return HttpResponse("\n".join(output))
+    latest = Post.objects.order_by("-pub_date")[:11]
+    return render(request, "index.html", {"posts": latest})
 
